84640f427a2f99940e0efa2986fb791e5c58913d
Roland Kajatin <roland.kajatin@gmail.com>
2023-05-07 17:58:48 +0200

    fixed broken demo page

--- src/client/DemoArea/DemoPage.tsx
+++ src/client/DemoArea/DemoPage.tsx
@@ -12,13 +12,27 @@ export default function GamePage({ user }: { user: User }) {
 
   const { data: tank } = useQuery(getTank);
 
-  const { data: state } = useQuery(getState);
+  const gameId = 39;
+  const { data: state } = useQuery(getState, { gameId: gameId });
 
   return (
     <div className="flex w-screen flex-col">
       <div className="flex w-screen flex-row gap-4">
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={async () => await spawnPlayers({gameid: 2})}>Spawn Players</button>
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={async () => await actionInGame({gameID: 2, action: '{"action":"move","info":{"q":1,"r":4}}' })}>
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={async () => await spawnPlayers({ gameid: gameId })}
+        >
+          Spawn Players
+        </button>
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={async () =>
+            await actionInGame({
+              gameID: gameId,
+              action: '{"action":"move","info":{"q":1,"r":4}}',
+            })
+          }
+        >
           Demo Action
         </button>
       </div>
